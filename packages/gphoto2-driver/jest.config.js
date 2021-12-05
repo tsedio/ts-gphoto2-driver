659b369dc438a1de4e5d18c72be9fbd3d9da1e8c
Romain Lenzotti <rom.lenzotti@gmail.com>
2021-12-05 09:14:23 +0100

    chore: Configure jest to run test per packages

--- packages/gphoto2-driver/jest.config.js
+++ packages/gphoto2-driver/jest.config.js
@@ -1,1 +1,11 @@
-module.exports = require("../../jest.config");
+module.exports = {
+  ...require("../../base.jest.config")(__dirname),
+  coverageThreshold: {
+    global: {
+      branches: 2.87,
+      functions: 4.98,
+      lines: 6.86,
+      statements: 6.77
+    }
+  }
+}
